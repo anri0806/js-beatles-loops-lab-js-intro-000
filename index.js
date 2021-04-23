2ab30dd34a8b96982a9379046103a29137f656b3
Anri Kawahara <anri0806@hotmail.co.jp>
2021-04-23 15:15:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let array = []
+
+  for (let i = 0; i < 4; i++) {
+  array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts() {
+return Math.random() >= 0.5;
+  while (johnLennonFacts()) {
+    return facts + "!!!";
+  }}
